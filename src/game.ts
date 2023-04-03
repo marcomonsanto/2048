10da1108b121ac98a989a9a18bbe1579677ff6ab
Marco Monsanto <marco.monsanto09@gmail.com>
2023-04-03 21:33:39 +0100

    add swipe top and bottom

--- src/game.ts
+++ src/game.ts
@@ -6,6 +6,8 @@ type Previous = {
   tileValue: number | undefined;
 };
 export type Board = Array<Array<number | undefined>>;
+type Callback = (board: Board, status: Status) => void;
+
 const TILE_INITIAL_VALUE = 1;
 
 class Game {
@@ -13,12 +15,9 @@ class Game {
   board: Board;
   emptySquares: string[] = [];
   status: Status = "inProgress";
-  callback: (board: Board, status: Status) => void;
+  callback: Callback;
 
-  constructor(
-    callback: (board: Board, status: Status) => void,
-    size: number = 6
-  ) {
+  constructor(callback: Callback, size: number = 6) {
     this.callback = callback;
     this.size = size;
     this.board = this.generateInitialBoard();
@@ -28,9 +27,8 @@ class Game {
   generateInitialBoard(): Board {
     let initialBoard: Board = [];
     for (let i = 0; i < this.size; i++) {
-      initialBoard.push([]);
+      initialBoard.push(Array(this.size).fill(undefined));
       for (let j = 0; j < this.size; j++) {
-        initialBoard[i].push(undefined);
         this.emptySquares.push(`${i}.${j}`);
       }
     }
@@ -134,7 +132,69 @@ class Game {
         break;
       }
       case "top": {
-        console.log("swipe top");
+        for (let i = 0; i <= this.size - 1; i++) {
+          let previous: Previous = {
+            isAvailable: false,
+            howMany: 0,
+            tileValue: undefined,
+          };
+          for (let j = 0; j <= this.size - 1; j++) {
+            let currentTileValue = this.board[j][i];
+            // No value in current tile
+            if (!currentTileValue) {
+              previous = {
+                isAvailable: true,
+                howMany: previous.isAvailable ? previous.howMany + 1 : 1,
+                tileValue: previous.tileValue,
+              };
+              continue;
+            }
+
+            // Value in current tile, but no space before and values are different
+            if (
+              !previous.isAvailable &&
+              previous.tileValue !== currentTileValue
+            ) {
+              previous = {
+                isAvailable: false,
+                howMany: 0,
+                tileValue: currentTileValue,
+              };
+              continue;
+            }
+
+            // MERGE: Value in current tile, no space before but values ARE equal
+            if (
+              // !previous.isAvailable &&
+              previous.tileValue === currentTileValue
+            ) {
+              const newValue = previous.tileValue + currentTileValue;
+              this.board[j - previous.howMany - 1][i] = newValue;
+              this.board[j][i] = undefined;
+
+              previous = {
+                isAvailable: true,
+                howMany: 1 + previous.howMany,
+                tileValue: undefined,
+              };
+              if (newValue === 2048) {
+                this.status = "win";
+                break;
+              }
+              continue;
+            }
+
+            // if value in tile and space before
+            this.board[j - previous.howMany][i] = currentTileValue;
+            this.board[j][i] = undefined;
+
+            previous = {
+              isAvailable: true,
+              howMany: previous.isAvailable ? previous.howMany : 1,
+              tileValue: currentTileValue,
+            };
+          }
+        }
         break;
       }
       case "right": {
@@ -204,6 +264,69 @@ class Game {
         break;
       }
       case "bottom": {
+        for (let i = 0; i <= this.size - 1; i++) {
+          let previous: Previous = {
+            isAvailable: false,
+            howMany: 0,
+            tileValue: undefined,
+          };
+          for (let j = this.size - 1; j >= 0; j--) {
+            let currentTileValue = this.board[j][i];
+            // No value in current tile
+            if (!currentTileValue) {
+              previous = {
+                isAvailable: true,
+                howMany: previous.isAvailable ? previous.howMany + 1 : 1,
+                tileValue: previous.tileValue,
+              };
+              continue;
+            }
+
+            // Value in current tile, but no space before and values are different
+            if (
+              !previous.isAvailable &&
+              previous.tileValue !== currentTileValue
+            ) {
+              previous = {
+                isAvailable: false,
+                howMany: 0,
+                tileValue: currentTileValue,
+              };
+              continue;
+            }
+
+            // MERGE: Value in current tile, no space before but values ARE equal
+            if (
+              // !previous.isAvailable &&
+              previous.tileValue === currentTileValue
+            ) {
+              const newValue = previous.tileValue + currentTileValue;
+              this.board[j + previous.howMany + 1][i] = newValue;
+              this.board[j][i] = undefined;
+
+              previous = {
+                isAvailable: true,
+                howMany: 1 + previous.howMany,
+                tileValue: undefined,
+              };
+              if (newValue === 2048) {
+                this.status = "win";
+                break;
+              }
+              continue;
+            }
+
+            // if value in tile and space before
+            this.board[j + previous.howMany][i] = currentTileValue;
+            this.board[j][i] = undefined;
+
+            previous = {
+              isAvailable: true,
+              howMany: previous.isAvailable ? previous.howMany : 1,
+              tileValue: currentTileValue,
+            };
+          }
+        }
         console.log("swipe bottom");
         break;
       }
